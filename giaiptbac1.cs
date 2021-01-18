792368c875a9438821e5e0e83b573eb3ad2062a6
Nguyễn Phương Thùy Linh <58435160+linhheo@users.noreply.github.com>
2021-01-18 16:18:54 +0700

    Add files via upload

---
+++ giaiptbac1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giaiptbac1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap he so a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap he so b:");
+            int b = int.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                if (b == 0)
+                    Console.WriteLine("Phuong trinh co vo so nghiem");
+                else
+                        Console.WriteLine("Phuong trinh vo nghiem");
+            } 
+            else
+            {
+                int ketqua = -b / a;
+                Console.WriteLine("Phuong trinh co nghiem la: {0}", ketqua.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ helloworld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ho va ten:");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Chao mung"+' '+ fullName +' '+"den voi mon hoc LTQL");
+        }
+    }
+}
